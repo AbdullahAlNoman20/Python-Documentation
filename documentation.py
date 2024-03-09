69d811f7e784a287db9e8d24b74a3885c1572b8c
Abdullah Al Noman <noman15-5387@diu.edu.bd>
2024-03-10 01:14:24 +0600

    Update Somthing

--- documentation.py
+++ documentation.py
@@ -1,4 +1,138 @@
-print("Hello Python")
+print("Hello World")
 
+
+# Some useful Library
+ 
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(","))
+
+
+
+
+
+# Variable Declaration
+Name = "Abdullah"
+print(Name)
+
+
+# Getting Input From User
+age = int(input("Enter Your Age "))
+print("Your age is",age)
+
+
+# Control statement
+if(age <= 18):
+    print("So, Ghumia Thako")
+
+else:
+    print("Vot Dite Jaw")
+
+
+
+# For Loop
+n = int(input("Enter Any Value"))
+list = []
+
+for i in range (1,n+1,2):
+    # 1 thake suru hoye n+1 porjonto jabe r protibar 2 kore barbe
+    list.append(i)
+
+print(list)
+
+
+for N in "Abdullah":
+  print(N)
+
+
+  for A in range(2, 30, 3):
+  print(A) 
+
+
+
+
+
+
+# Break & Continue
+
+
+# Function 
+
+def function():
+    print("Someone Call me ")
+
+function()
+
+
+
+
+
+
+# List Operation
 list = [1,2,3,4,5]
 print(list)
+
+
+print(list[1])
+print(list[-1])
+
+
+list[1] = 20
+print(list)
+
+
+list.append(500)
+print(list)
+
+
+list.remove(5)
+print(list)
+
+
+for L in list:
+  print(L)
+
+
+newlist = []
+
+for x in list:
+    if 3 in x:
+        newlist.append(x)
+
+print(newlist)
+
+
+
+list.sort()
+print(list)
+
+
+
+mylist = list.copy()
+print(mylist)
+
+
+
+list2 = [1, 2, 3]
+
+list3 = list + list2
+print(list3)
+
+
+# Tuples
+
+# Set
+
+
+# dictionary
+
+
